2a4801e3e18de3ec2d7aa3ab86d52b0119a6fe6f
Steven <richlife1220@gmail.com>
2024-11-30 16:08:37 +0800

    baseSet 修正與第1次更改 README

--- README.md
+++ README.md
@@ -1,35 +1,43 @@
-# kkbox-project
+# KK music
 
-This template should help get you started developing with Vue 3 in Vite.
+## 簡介
 
-## Recommended IDE Setup
+> 此 KK music 專案，主要使用 Vite 建立的 Vue3 Composition API 搭配 Bootstrap 5 和 KKBOX Open API 製作
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+## 網址
 
-## Customize configuration
+[https://steven-1220.github.io/kkmusic/#/](https://steven-1220.github.io/kkmusic/#/)
 
-See [Vite Configuration Reference](https://vite.dev/config/).
+## 網站功能
 
-## Project Setup
+- 各類型熱門歌曲最新排行榜試聽
+- 各種類新發行專輯試聽
+- 4大種類(華語、西洋、韓語、日語)熱門歌曲 YouTube 完整聆聽
+- 搜尋功能
+  - 詳細相關專輯頁面
+  - 詳細相關單曲頁面
+  - 詳細相關歌單頁面
 
-```sh
-npm install
-```
+## 使用技術
 
-### Compile and Hot-Reload for Development
+- Vite
+- Vue3 Composition API
+- Vue Router
+- Pinia
+- KKBOX OAuth 2.0 Token API
+- YouTube Data API
+- Bootstrap5
+- JavaScript
+- SCSS
+- RWD
 
-```sh
-npm run dev
-```
+## 使用插件
 
-### Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
+- axios
+- swiper
+- vue-loading-overlay
+- vuejs-paginate-next
+- vue3-youtube
+- sweetalert2
+- aos
+- query-string
--- src/stores/baseSet.js
+++ src/stores/baseSet.js
@@ -1,7 +1,7 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
-import queryString from 'query-string'
-import axios from 'axios'
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+import queryString from 'query-string';
+import axios from 'axios';
 
 export const useBaseSet = defineStore('baseSet', () => {
   // State
@@ -10,36 +10,39 @@ export const useBaseSet = defineStore('baseSet', () => {
       Accept: 'application/x-www-form-urlencoded',
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-  })
+  });
 
   const oauth = ref({
     grant_type: import.meta.env.VITE_API_GRANTTYPE,
     client_id: import.meta.env.VITE_API_CLINETID,
     client_secret: import.meta.env.VITE_API_CLIENTSECRET,
-  })
+  });
+
+  const corsProxy = 'https://corsproxy.io/?';
+  const apiUrl = `${corsProxy}https://account.kkbox.com/oauth2/token`;
 
   const getConfigAuthToken = async () => {
     try {
-      let configAuth = {}
+      let configAuth = {};
       const res = await axios.post(
-        '/api',
+        apiUrl,
         queryString.stringify(oauth.value),
         config.value,
-      )
+      );
 
-      console.log('getConfigAuthToken 成功', res)
+      console.log('getConfigAuthToken success', res);
 
       configAuth = {
         headers: {
           Authorization: `Bearer ${res.data.access_token}`,
         },
-      }
+      };
 
-      return configAuth
+      return configAuth;
     } catch (error) {
-      console.log('getConfigAuthToken 錯誤 :', error.response)
+      console.log('getConfigAuthToken 錯誤 :', error.response);
     }
-  }
+  };
 
-  return { getConfigAuthToken }
-})
+  return { getConfigAuthToken };
+});
